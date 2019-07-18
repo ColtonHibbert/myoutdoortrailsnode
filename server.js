5ccaa49406a9dd3f0ab155c7e8c76669a188f9fc
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-07-18 15:50:34 -0600

    working on logging json to front end

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
 
-app.listen(3001)
+const searchfield = require('./controllers/searchfield.js');
+
+const database = {
+    searchfield: 'blank',
+}
+
+app.use(bodyparser.json());
+app.use(cors());
+
+
+app.post('/searchfield', (req,res) => {
+    console.log(req.body)
+    const data = req.body.searchField
+    res.json(`here is the ${data}`)
+    console.log(`here is the ${data}`)
+    })
+app.get('/', (req,res) => res.send('this is working'))
+
+app.listen(3001, () => console.log('running on port 3001'))

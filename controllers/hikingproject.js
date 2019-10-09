4e5cc8796fdf321ca26f0bea0c06a1270f57aa3f
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-08 21:34:43 -0600

    fetch

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -9,7 +9,8 @@ const handleHikingProject = (req, res ) => {
         method: "POST",
         headers: {'Content-type': 'application/json'},
     })
-    .then(res => res)
+    .then(res => res.jon())
+    .then(data => console.log(data))
     .catch(err => res.status(400).json('hiking project request failed'))
 }
 

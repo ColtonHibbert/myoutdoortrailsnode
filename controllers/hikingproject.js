83936ee1e56ab4911274cd3253b6d3383478b9ac
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-08 21:19:32 -0600

    fetch

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -5,7 +5,10 @@ const handleHikingProject = (req, res ) => {
     if(!lat  || !lon) {
         return res.json(`unable to get hiking project api data`)
     }
-    fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`)
+    fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`, {
+        method: "POST",
+        headers: {'Content-type': 'application/json'}
+    })
     .then(res => res.json())
     .catch(err => res.status(400).json('hiking project request failed'))
 }

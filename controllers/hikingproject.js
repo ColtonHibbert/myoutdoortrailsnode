477938d9b8c3179a929e73a180344d60c2ac3db7
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-09 22:05:59 -0600

    working on request

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -6,14 +6,22 @@ const handleHikingProject = (req, res ) => {
         return res.json(`unable to get hiking project api data`)
     }
     console.log(lat, lon)
-    fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
-    // , {
-    //     method: "POST",
-    //     headers: {'Content-type': 'application/json'},
-    //     mode: "no-cors" 
-    // }
-    ).then(data => console.log(data.json()))
-    .catch(err => res.status(400).json('hiking project request failed'))
+    async function hikes() {
+        const getHikes = await fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`);
+        return getHikes;
+    }
+    const hikesValues = hikes();
+    console.log(hikesValues);
+    // fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
+    // // , {
+    // //     method: "POST",
+    // //     headers: {'Content-type': 'application/json'},
+    // //     mode: "no-cors" 
+    // // }
+    // ).then(data => console.log(data.json()))
+    // .catch(err => res.status(400).json('hiking project request failed'))
+    // try async, send res at end, put await value in the res, maybe res.send 
+
 }
 
 module.exports = {

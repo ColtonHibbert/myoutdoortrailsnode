75bc810356c09daf664ca7e5ff3c2f33e57d1279
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-10 22:16:42 -0600

    working on request

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -19,16 +19,17 @@ const handleHikingProject = (req, res ) => {
     //     return getHikes;
     // }
     // const hikesValues = hikes();
-    res.json("works")
+    //res.json("works")
     //console.log(hikesValues);
-    // fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
-    // // , {
-    // //     method: "POST",
-    // //     headers: {'Content-type': 'application/json'},
-    // //     mode: "no-cors" 
-    // // }
-    // ).then(data => console.log(data.json()))
-    // .catch(err => res.status(400).json('hiking project request failed'))
+    fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
+    , {
+        method: "POST",
+        headers: {'Content-type': 'application/json',
+                  'Access-Control-Allow-Origin': '*'
+        }
+    }
+    ).then(data => data.json())
+    .catch(err => res.status(400).json('hiking project request failed'))
     // try async, send res at end, put await value in the res, maybe res.send 
 
 }

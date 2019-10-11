c6ed15acacc6d6c3f1e3e210ad98b2e6ace7f28d
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-11 16:58:12 -0600

    ssl working testing request

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -21,14 +21,14 @@ const handleHikingProject = (req, res ) => {
     // const hikesValues = hikes();
     //res.json("works")
     //console.log(hikesValues);
-    //fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
+    fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
     // , {
     //     method: "POST",
     //     headers: {
     //               'Access-Control-Allow-Origin': '*'
     //     }
     // }
-    //).then(res => console.log(res))
+    ).then(res => console.log(res))
     //.catch(err => res.status(400).json('hiking project request failed'))
     // try async, send res at end, put await value in the res, maybe res.send 
 

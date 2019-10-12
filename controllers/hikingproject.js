86adbae42be98b8577c0acf59f8782a43dac3237
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-10-12 10:55:53 -0600

    working on request

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -28,9 +28,8 @@ const handleHikingProject = (req, res ) => {
     //               'Access-Control-Allow-Origin': '*'
     //     }
     // }
-    ).then(res =>  {
-        console.log(JSON.parse(res.text()))
-    })
+    ).then(res => res.json())
+    .then(data => console.log(data))
     console.log("after hiking project fetch")
     //.catch(err => res.status(400).json('hiking project request failed'))
     // try async, send res at end, put await value in the res, maybe res.send 

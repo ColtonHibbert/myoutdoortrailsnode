3a3f3bddcf9d27430ee7206effff858e081c71b6
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-09-25 13:34:26 -0600

    remove spacing and comments

--- controllers/hikingproject.js
+++ controllers/hikingproject.js
@@ -5,15 +5,10 @@ const handleHikingProject = (req, res ) => {
     if(!lat  || !lon) {
         return res.json(`unable to get hiking project api data`)
     }
-    //console.log(lat, lon)
-    //console.log(hikesValues);
     fetch(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${process.env.HPKEY}`
     ).then(data => data.json())
     .then( (trails) => res.send(trails))
-    //console.log("after hiking project fetch")
     .catch(err => res.status(400).json('hiking project request failed'))
-
-
 }
 
 module.exports = {
